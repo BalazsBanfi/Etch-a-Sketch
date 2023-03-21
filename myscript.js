a9330e49e392ebbe0b1e6064240eb1db4bb025c5
bally4h <banfibalazs81@gmail.com>
2023-03-21 18:06:28 +0100

    Css grid size default value in variable

--- myscript.js
+++ myscript.js
@@ -1,6 +1,9 @@
+let rows = 0;
+
+rows = 2;
+
 const container = document.querySelector('#container');
 const divGrid = document.createElement('div');
 divGrid.classList.add('divGrid');
 
-//divGrid.textContent = 'Hello Word!';
 container.appendChild(divGrid);
