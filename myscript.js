bcbc1be71919e16d59aafe859a99f06845ae35aa
bally4h <banfibalazs81@gmail.com>
2023-03-21 19:58:57 +0100

    Reset btn change square size in css variable

--- myscript.js
+++ myscript.js
@@ -1,9 +1,25 @@
-let rows = 10;
-
 const container = document.querySelector('#container');
+let rows = 'calc(480px / 10)';
 
-for (let i = 0; i < (rows * rows); i++) {
-    const divGrid = document.createElement('div');
-    divGrid.classList.add('divGrid');
-    container.appendChild(divGrid);
+// reset button set
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click',  () => {
+    resetFunction(16);
+});
+
+const r = document.querySelector(':root');
+function myFunction_set() {
+    r.style.setProperty('--grid-size', rows);
+}
+
+
+
+function resetFunction(rows) {
+    myFunction_set();
+    document.querySelectorAll('.divGrid').forEach(el => el.remove());
+    for (let i = 0; i < (rows * rows); i++) {
+        const divGrid = document.createElement('div');
+        divGrid.classList.add('divGrid');
+        container.appendChild(divGrid);
+    }
 }
