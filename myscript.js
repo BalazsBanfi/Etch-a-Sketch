4c9e46f83794df66182df55287df1c44445c3a82
bally4h <banfibalazs81@gmail.com>
2023-03-21 17:52:16 +0100

    Base html css, js append 1 square

---
+++ myscript.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('#container');
+const divGrid = document.createElement('div');
+divGrid.classList.add('divGrid');
+
+//divGrid.textContent = 'Hello Word!';
+container.appendChild(divGrid);
